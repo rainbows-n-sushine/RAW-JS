7e2a787358ea8fd16ac97c547aa1682261f0ed94
rainbows-n-sunshine <jalaladdisu@gmail.com>
2025-05-03 21:17:29 +0300

    basic add and minus work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 //be clickable for both plus button and minus
 //change the values of the displayed nuumber accordingly
 
-const body=document.body;
 const counter=document.getElementById('counter')
 const plusBtn=document.getElementById('plusBtn')
 const minusBtn=document.getElementById('minusBtn')
+
+let count=0;
+plusBtn.addEventListener('click',()=>{
+    count=count+1;
+    counter.innerText=count;
+ 
+})
+
+minusBtn.addEventListener("click",()=>{
+
+    count=count-1;
+    counter.innerText=count;
+
+})
