46529dad100d90b55dcce796236012a13a8a06ab
jaddis53 <jayaddisu@gmail.com>
2025-05-03 20:50:06 +0300

    CSS done :)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//be clickable for both plus button and minus
+//change the values of the displayed nuumber accordingly
+
+const body=document.body;
+const counter=document.getElementById('counter')
+const plusBtn=document.getElementById('plusBtn')
+const minusBtn=document.getElementById('minusBtn')
